890a955435d5651bf9055fd2d1c98bc33002d2b1
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2024-10-16 19:58:24 -0300

    último exercicio feito

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main {
         String resultado4 = exercicio4("Teste");
         System.out.println("Resultado 4: " + resultado4);
 
-        int resultado5 = michaelNatti(5);
+        int resultado5 = sequenciador(5);
         System.out.println("Resultado 5: " + resultado5);
 
         int resultado6 = ackerman(3, 3);
@@ -24,6 +24,16 @@ public class Main {
         int[] vetor = {1, 2, 3, 4, 5};
         int resultado7 = soma(vetor);
         System.out.println("Resultado 7: " + resultado7);
+
+        boolean resultado8 = palindromo("arara");
+        System.out.println("Resultado 8: " + resultado8);
+
+        ArrayList<String> resultado9 = permutacoes(3);
+        System.out.println("Resultado 9: " + resultado9);
+
+        int resultado10 = sequenciaFibonacci(0, 1, 5);
+        System.out.println("Resultado 10: " + resultado10);
+        
     }
 
     private static int exercicio1(int numeroDeVezes, int multiplicando) {
@@ -66,10 +76,10 @@ public class Main {
         return str.charAt(pos) + exercicio4(str, pos - 1);
     }
 
-    private static int michaelNatti(int numeroUsado){
+    private static int sequenciador(int numeroUsado){
         if(numeroUsado <= 1) return 1;
         if(numeroUsado == 2) return numeroUsado;
-        return 2* michaelNatti(numeroUsado - 1) + 3*michaelNatti(numeroUsado - 2); 
+        return 2* sequenciador(numeroUsado - 1) + 3*sequenciador(numeroUsado - 2); 
     }
 
     private static int ackerman(int m, int n) {
@@ -97,20 +107,30 @@ public class Main {
         return palindromo(str, inicio + 1, fim - 1);
     }
 
-    private static ArrayList<String> subconjuntos(int n){
-        if (n > 26) n = 26;
-
-        return subconjuntos(n, 0, new ArrayList<>());
-    }
-
-    private static ArrayList<String> subconjuntos(int n, int cont, ArrayList<String> lista){
-        if(con) return lista;
-        ArrayList<String> novaLista = new ArrayList<>();
-        for(String str : lista){
-            novaLista.add(str + (char)('a' + cont));
+    private static ArrayList<String> permutacoes(int n) {
+        ArrayList<String> resultado = new ArrayList<>();
+        String str = "";
+        for(int i = 0; i < n; i++) {
+            str += (char) ('A' + i);
         }
-        lista.addAll(novaLista);
-        lista.add("" + (char)('a' + cont));
-        return subconjuntos(n, cont + 1, lista);
+        permutacoes("", str, resultado);
+        return resultado;
     }
+
+    private static void permutacoes(String prefixo, String str, ArrayList<String> resultado) {
+        int n = str.length();
+        if (n == 0) {
+            resultado.add(prefixo);
+        } else {
+            for (int i = 0; i < n; i++) {
+                permutacoes(prefixo + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), resultado);
+            }
+        }
+    }
+
+    private static int sequenciaFibonacci(int f0, int f1, int n) {
+        if(n == 0) return f0;
+        if(n == 1) return f1;
+        return sequenciaFibonacci(f0, f1, n - 1) + sequenciaFibonacci(f0, f1, n - 2);
+    } 
 }
